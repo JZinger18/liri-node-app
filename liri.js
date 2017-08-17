dbc8290b1c1564da3e93ba15f22907e84cf66b19
Jordan Zinger <jzinger18@gmail.com>
2017-08-17 17:30:19 -0400

    do what it says function

--- liri.js
+++ liri.js
@@ -1,27 +1,6 @@
 
-// // fs is a core Node package for reading and writing files
-// var fs = require("fs");
 
-// // This block of code will read from the "movies.txt" file.
-// // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-// // The code will store the contents of the reading inside the variable "data"
-// fs.readFile("random.txt", "utf8", function(error, tData) {
 
-//   // If the code experiences any errors it will log the error to the console.
-//   if (error) {
-//     return console.log(error);
-//   }
-
-//   // We will then print the contents of data
-//   console.log(tData);
-
-//   // Then split it by commas (to make it more readable)
-//   var dataArr = tData.split(",");
-
-//   // We will then re-display the content as an array for later use.
-//   console.log(dataArr);
-
-// });
 
 
 
@@ -31,13 +10,13 @@
 
 // capture commands from command  line argument 
 var call = process.argv[2]
-// var prompt = process.argv[3]
+
 
 
 // Take in the command line arguments
 var nodeArgs = process.argv;
 
-// Create an empty string for holding the address
+// Create an empty string for holding the input
 var input = "";
 
 // Capture all the words in the statemnt
@@ -47,9 +26,6 @@ for (var i = 3; i < nodeArgs.length; i++) {
   input = input + " " + nodeArgs[i];
 
 }
-
-console.log(input)
-
 
 
 
@@ -67,6 +43,11 @@ var command = {
 
 	'my-tweets': function() {
 		tweet()
+	},
+
+	'do-what-it-says': function() {
+		initDoWhatItSays()
+
 	}
 }
 
@@ -74,6 +55,8 @@ var command = {
 //load the NPM Package inquirer
 var Spotify = require('node-spotify-api');
 
+
+// function for spotify method
 initSpotify = function(input) {
 
 	if(input.length < 1) {
@@ -81,38 +64,38 @@ initSpotify = function(input) {
 	}
 
 
-var spotify = new Spotify({
-	id: '1b919a2ba2a5457d8dc91c5d49bacf3a',
-	secret: 'e659a8eee6b8455db4c53370a6b3886a'
-})
-
-spotify
-	.search({type:'track',query:input,limit:1})
-	.then(function(response) {
-		data = response
-		// console.log(data.tracks.items[0].preview_url)
-		artist = data.tracks.items[0].album.artists[0].name
-		song = data.tracks.items[0].name
-		link = data.tracks.items[0].preview_url
-		album = data.tracks.items[0].album.name
-		clean()
-		console.log('Artist(s): ' + artist)
-		console.log('Song: ' + song)
-		console.log('Preview Link: ' + link)
-		console.log('Album: ' + album)
-		clean()
-	})
-	.catch(function(err) {
-		console.log(err)
+	var spotify = new Spotify({
+		id: '1b919a2ba2a5457d8dc91c5d49bacf3a',
+		secret: 'e659a8eee6b8455db4c53370a6b3886a'
 	})
 
-}
+	spotify
+		.search({type:'track',query:input,limit:1})
+		.then(function(response) {
+			data = response
+			// console.log(data.tracks.items[0].preview_url)
+			artist = data.tracks.items[0].album.artists[0].name
+			song = data.tracks.items[0].name
+			link = data.tracks.items[0].preview_url
+			album = data.tracks.items[0].album.name
+			clean()
+			console.log('Artist(s): ' + artist)
+			console.log('Song: ' + song)
+			console.log('Preview Link: ' + link)
+			console.log('Album: ' + album)
+			clean()
+		})
+		.catch(function(err) {
+			console.log(err)
+		})
+
+	}
 
 
 
 
 
-
+//used to add line spacing
 function clean () {
 	console.log('====================================')
 }
@@ -120,7 +103,7 @@ function clean () {
 
 
 
-http://www.omdbapi.com/?t=remember+the+titans
+
 
 // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
 var request = require("request");
@@ -134,13 +117,8 @@ initMovie = function(input) {
 // Then run a request to the OMDB API with the movie specified
 request("http://www.omdbapi.com/?t=" + input + "&apikey=40e9cece", function(error, response, body) {
 
-  // If the request is successful (i.e. if the response status code is 200)
-  // if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log(body);
-     title = JSON.parse(body).Title
+    
+    title = JSON.parse(body).Title
     year = JSON.parse(body).Released
     imdbRating = JSON.parse(body).imdbRating
     // rottenRating = JSON.parse(body).imdbRating
@@ -157,43 +135,68 @@ request("http://www.omdbapi.com/?t=" + input + "&apikey=40e9cece", function(erro
     console.log('Featured Actors: ' + actors)
     clean()
 
-  // }
-});
+	});
 
 }
 
 
+//twitter function 
 tweet = function() {
-	clean()
 
+	var twitterOath = require("./key.js");
 
-
-var twitterOath = require("./key.js");
-
-var Twitter = require('twitter');
- 
-var client = new Twitter(
-twitterOath.twitterKeys
-);
- 
-var params = {screen_name: 'jzinger18'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for (var i=0;i<20;i++) {
-    	
-    	console.log('Tweet: ' + tweets[i].text +' \n Timestamp: ' + tweets[i].created_at);
-    	clean()
-}
-  }
-});
+	var Twitter = require('twitter');
+	 
+	var client = new Twitter(
+	twitterOath.twitterKeys
+	);
+	 
+	var params = {screen_name: 'jzinger18'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    for (var i=0;i<20;i++) {
+	    	
+	    	console.log('Tweet: ' + tweets[i].text +' \n Timestamp: ' + tweets[i].created_at);
+	    	clean()
+	}
+	  	}	
+	});
 
 }
 
 
 
+// Do what it says function to be called by command line
+initDoWhatItSays = function() {
+
+
+	//fs is a core Node package for reading and writing files
+	var fs = require("fs");
+
+	// The code will store the contents of the reading inside the variable "data"
+	fs.readFile("random.txt", "utf8", function(error, tData) {
+
+	  // If the code experiences any errors it will log the error to the console.
+	  if (error) {
+	    return console.log(error);
+	  }
+
+  	// data stored in array for later use.
+	var dataArr = tData.split(",");
+	tempCommand = dataArr[0];
+  	tempInput = dataArr[1];
+  	input = tempInput
+  	command[tempCommand]()
+
+	});
+
+}
 
 
 
+
+
+//invoke functions stored in objects for command line commands
 command[call]()
 
 
